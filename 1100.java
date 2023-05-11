c76e84802a1cede0b6e506edfed1dcc445b0beb9
minhe8564 <91974925+minhe8564@users.noreply.github.com>
2023-05-12 01:17:58 +0900

    하얀 칸

---
+++ 1100.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        for(int i = 0; i < 8; i++){
+            String s = br.readLine();
+            for(int j = 0; j < 8; j++){
+                if((i+j) % 2 == 0 && s.charAt(j) == 'F'){
+                    count++;
+                }
+            }
+        }
+        
+        System.out.print(count);
+        
+    }
+}
