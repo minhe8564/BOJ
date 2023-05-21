a29e19d2ce02099a7865cd28edc84cc6f92ca36a
minhe8564 <91974925+minhe8564@users.noreply.github.com>
2023-05-22 00:15:14 +0900

    달팽이는 올라가고 싶다

---
+++ 2869.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		br.close();
+		int day = ((V - B - 1) / (A - B)) + 1;
+		System.out.print(day);
+	}
+}
